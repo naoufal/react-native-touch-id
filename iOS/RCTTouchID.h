6ee1edef86fb997dc9097642247a08f2b566b9de
Naoufal Kadhom <naoufalkadhom@gmail.com>
2015-06-02 23:45:37 -0400

    initial commit

---
+++ iOS/RCTTouchID.h
@@ -0,0 +1,13 @@
+//
+//  RCTTouchID.h
+//  TouchID
+//
+//  Created by Naoufal Kadhom on 2015-06-02.
+//  Copyright (c) 2015 Facebook. All rights reserved.
+//
+
+#import "RCTBridgeModule.h"
+
+@interface RCTTouchID : NSObject <RCTBridgeModule>
+
+@end
