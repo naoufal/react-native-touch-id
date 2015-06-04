d689c36e43b0ef26440b47179b0a54e0a288dc8b
Naoufal Kadhom <naoufalkadhom@gmail.com>
2015-06-04 09:15:40 -0400

    promisify authenticate and add better errors

--- TouchID.ios.js
+++ TouchID.ios.js
@@ -6,13 +6,41 @@
 
 var NativeTouchID = require('NativeModules').TouchID;
 var invariant = require('invariant');
+var ERRORS = require('./data/errors');
 
 /**
  * High-level docs for the TouchID iOS API can be written here.
  */
 
 var TouchID = {
-  authenticate: NativeTouchID.authenticate
+  authenticate() {
+    return new Promise(function(resolve, reject) {
+      NativeTouchID.authenticate(function(error, success) {
+        // Return error if rejected
+        if (error) {
+          return reject(createError(error.message));
+        }
+
+        resolve(true);
+      });
+    });
+  }
 };
+
+function createError(error) {
+  var details = ERRORS[error];
+  details.name = error;
+
+  return new TouchIDError(error, details);
+}
+
+function TouchIDError(name, details) {
+  this.name = name || 'TouchIDError';
+  this.message = details.message || 'Touch ID Error';
+  this.details = details || {};
+}
+
+TouchIDError.prototype = Object.create(Error.prototype);
+TouchIDError.prototype.constructor = TouchIDError;
 
 module.exports = TouchID;
---
+++ data/errors.js
@@ -0,0 +1,29 @@
+module.exports = {
+  "LAErrorAuthenticationFailed": {
+    message: "Authentication was not successful because the user failed to provide valid credentials."
+  },
+  "LAErrorUserCancel": {
+    message: "Authentication was canceled by the user—for example, the user tapped Cancel in the dialog."
+  },
+  "LAErrorUserFallback": {
+    message: "Authentication was canceled because the user tapped the fallback button (Enter Password)."
+  },
+  "LAErrorSystemCancel": {
+    message: "Authentication was canceled by system—for example, if another application came to foreground while the authentication dialog was up."
+  },
+  "LAErrorPasscodeNotSet": {
+    message: "Authentication could not start because the passcode is not set on the device."
+  },
+  "LAErrorTouchIDNotAvailable": {
+    message: "Authentication could not start because Touch ID is not available on the device"
+  },
+  "LAErrorTouchIDNotEnrolle": {
+    message: "Authentication could not start because Touch ID has no enrolled fingers."
+  },
+  "RCTTouchIDUnknownError": {
+    message: "Could not authenticate for an unknown reason."
+  },
+  "RCTTouchIDNotSupported": {
+    message: "Device does not support Touch ID."
+  }
+};
