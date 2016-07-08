1991b2bec08989f7e7075330c10978fc085ce718
Kishan Patel <kishcoin@gmail.com>
2016-07-08 00:21:06 -0400

    fixed exporting confusion
    Error references in #14

--- TouchID.ios.js
+++ TouchID.ios.js
@@ -14,7 +14,7 @@ console.log(NativeModules);
  * High-level docs for the TouchID iOS API can be written here.
  */
 
-export default {
+module.exports = {
   isSupported() {
     return new Promise((resolve, reject) => {
       NativeTouchID.isSupported(error => {
