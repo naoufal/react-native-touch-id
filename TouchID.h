d2b28a8b2532f9810c830707bcb96a60163d7291
Enguerrand des Vaux <enguerrand.desvaux@gmail.com>
2017-01-26 17:43:47 +1000

    Update TouchID.h

--- TouchID.h
+++ TouchID.h
@@ -1,4 +1,11 @@
+// import RCTBridgeModule
+#if __has_include(<React/RCTBridgeModule.h>)
+#import <React/RCTBridgeModule.h>
+#elif __has_include("RCTBridgeModule.h")
 #import "RCTBridgeModule.h"
+#else
+#import "React/RCTBridgeModule.h"   // Required when used as a Pod in a Swift project
+#endif
 
 @interface TouchID : NSObject <RCTBridgeModule>
 
