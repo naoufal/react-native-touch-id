4cfbaaf32ff5e3efe6befaef6a81e3727bd557f0
Ian Outterside <ian@komododigital.co.uk>
2018-03-12 16:03:42 +0000

    Ensure we check for functionality without biometrics as well

--- TouchID.m
+++ TouchID.m
@@ -10,7 +10,7 @@ RCT_EXPORT_METHOD(isSupported: (RCTResponseSenderBlock)callback)
     LAContext *context = [[LAContext alloc] init];
     NSError *error;
 
-    if ([context canEvaluatePolicy:LAPolicyDeviceOwnerAuthenticationWithBiometrics error:&error]) {
+    if ([context canEvaluatePolicy:LAPolicyDeviceOwnerAuthentication error:&error]) {
         callback(@[[NSNull null], [self getBiometryType:context]]);
         // Device does not support TouchID
     } else {
@@ -26,7 +26,7 @@ RCT_EXPORT_METHOD(authenticate: (NSString *)reason
     NSError *error;
 
     // Device has TouchID
-    if ([context canEvaluatePolicy:LAPolicyDeviceOwnerAuthenticationWithBiometrics error:&error]) {
+    if ([context canEvaluatePolicy:LAPolicyDeviceOwnerAuthentication error:&error]) {
         // Attempt Authentification
         [context evaluatePolicy:LAPolicyDeviceOwnerAuthentication
                 localizedReason:reason
