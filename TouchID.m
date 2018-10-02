51ede1dfd5b3ef3987dc28d856d823a0178549dc
Ian Outterside <ian@komododigital.co.uk>
2018-10-02 09:10:26 +0100

    Update handler to clearly prefer biometrics, but fallback to PIN

--- TouchID.m
+++ TouchID.m
@@ -11,10 +11,14 @@ RCT_EXPORT_METHOD(isSupported: (RCTResponseSenderBlock)callback)
     LAContext *context = [[LAContext alloc] init];
     NSError *error;
 
-    if ([context canEvaluatePolicy:LAPolicyDeviceOwnerAuthentication error:&error]) {
+    if ([context canEvaluatePolicy:LAPolicyDeviceOwnerAuthenticationWithBiometrics error:&error]) {
         callback(@[[NSNull null], [self getBiometryType:context]]);
-        // Device does not support TouchID
-    } else {
+        
+    } else if ([context canEvaluatePolicy:LAPolicyDeviceOwnerAuthentication error:&error]) {
+        callback(@[[NSNull null], [self getBiometryType:context]]);
+    }
+    // Device does not support FaceID / TouchID / Pin
+    else {
         callback(@[RCTMakeError(@"RCTTouchIDNotSupported", nil, nil)]);
         return;
     }
@@ -33,69 +37,90 @@ RCT_EXPORT_METHOD(authenticate: (NSString *)reason
     }
 
     // Device has TouchID
-    if ([context canEvaluatePolicy:LAPolicyDeviceOwnerAuthentication error:&error]) {
+    if ([context canEvaluatePolicy:LAPolicyDeviceOwnerAuthenticationWithBiometrics error:&error]) {
+        // Attempt Authentification
+        [context evaluatePolicy:LAPolicyDeviceOwnerAuthenticationWithBiometrics
+                localizedReason:reason
+                          reply:^(BOOL success, NSError *error)
+         {
+             [self handleAttemptToUseDeviceIDWithSuccess:success error:error callback:callback];
+         }];
+
+        // Device does not support TouchID
+    } else if ([context canEvaluatePolicy:LAPolicyDeviceOwnerAuthentication error:&error]) {
         // Attempt Authentification
         [context evaluatePolicy:LAPolicyDeviceOwnerAuthentication
                 localizedReason:reason
                           reply:^(BOOL success, NSError *error)
          {
-             if (success) { // Authentication Successful
-                 callback(@[[NSNull null], @"Authenticated with Touch ID."]);
-             } else if (error) { // Authentication Error
-                 NSString *errorReason;
-
-                 switch (error.code) {
-                     case LAErrorAuthenticationFailed:
-                         errorReason = @"LAErrorAuthenticationFailed";
-                         break;
-
-                     case LAErrorUserCancel:
-                         errorReason = @"LAErrorUserCancel";
-                         break;
-
-                     case LAErrorUserFallback:
-                         errorReason = @"LAErrorUserFallback";
-                         break;
-
-                     case LAErrorSystemCancel:
-                         errorReason = @"LAErrorSystemCancel";
-                         break;
-
-                     case LAErrorPasscodeNotSet:
-                         errorReason = @"LAErrorPasscodeNotSet";
-                         break;
-
-                     case LAErrorTouchIDNotAvailable:
-                         errorReason = @"LAErrorTouchIDNotAvailable";
-                         break;
-
-                     case LAErrorTouchIDNotEnrolled:
-                         errorReason = @"LAErrorTouchIDNotEnrolled";
-                         break;
-
-                     default:
-                         errorReason = @"RCTTouchIDUnknownError";
-                         break;
-                 }
-
-                 NSLog(@"Authentication failed: %@", errorReason);
-                 callback(@[RCTMakeError(errorReason, nil, nil)]);
-             } else { // Authentication Failure
-                 callback(@[RCTMakeError(@"LAErrorAuthenticationFailed", nil, nil)]);
-             }
+             [self handleAttemptToUseDeviceIDWithSuccess:success error:error callback:callback];
          }];
-
-        // Device does not support TouchID
-    } else {
+    }
+    else {
         callback(@[RCTMakeError(@"RCTTouchIDNotSupported", nil, nil)]);
         return;
+    }
+}
+
+- (void)handleAttemptToUseDeviceIDWithSuccess:(BOOL)success error:(NSError *)error callback:(RCTResponseSenderBlock)callback {
+    if (success) { // Authentication Successful
+        callback(@[[NSNull null], @"Authenticated with Touch ID."]);
+    } else if (error) { // Authentication Error
+        NSString *errorReason;
+        
+        switch (error.code) {
+            case LAErrorAuthenticationFailed:
+                errorReason = @"LAErrorAuthenticationFailed";
+                break;
+                
+            case LAErrorUserCancel:
+                errorReason = @"LAErrorUserCancel";
+                break;
+                
+            case LAErrorUserFallback:
+                errorReason = @"LAErrorUserFallback";
+                break;
+                
+            case LAErrorSystemCancel:
+                errorReason = @"LAErrorSystemCancel";
+                break;
+                
+            case LAErrorPasscodeNotSet:
+                errorReason = @"LAErrorPasscodeNotSet";
+                break;
+                
+            case LAErrorTouchIDNotAvailable:
+                errorReason = @"LAErrorTouchIDNotAvailable";
+                break;
+                
+            case LAErrorTouchIDNotEnrolled:
+                errorReason = @"LAErrorTouchIDNotEnrolled";
+                break;
+                
+            default:
+                errorReason = @"RCTTouchIDUnknownError";
+                break;
+        }
+        
+        NSLog(@"Authentication failed: %@", errorReason);
+        callback(@[RCTMakeError(errorReason, nil, nil)]);
+    } else { // Authentication Failure
+        callback(@[RCTMakeError(@"LAErrorAuthenticationFailed", nil, nil)]);
     }
 }
 
 - (NSString *)getBiometryType:(LAContext *)context
 {
     if (@available(iOS 11, *)) {
-        return (context.biometryType == LABiometryTypeFaceID) ? @"FaceID" : @"TouchID";
+        if (context.biometryType == LABiometryTypeFaceID) {
+            return @"FaceID";
+        }
+        else if (context.biometryType == LABiometryTypeTouchID) {
+            return @"TouchID";
+        }
+        else if (context.biometryType == LABiometryNone) {
+            return @"None";
+        }
     }
 
     return @"TouchID";
