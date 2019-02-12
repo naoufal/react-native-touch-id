f3357b3a727dbfd7d9a2397125fe3eceb3513235
Gabriele Prestifilippo <gabry.501@hotmail.it>
2019-02-12 09:14:08 +0100

    Indent new files

--- android/src/main/java/com/rnfingerprint/FingerprintNoDialog.java
+++ android/src/main/java/com/rnfingerprint/FingerprintNoDialog.java
@@ -1,40 +1,41 @@
 package com.rnfingerprint;
 
 import android.hardware.fingerprint.FingerprintManager;
+
 import com.facebook.react.bridge.ReactApplicationContext;
 
 public class FingerprintNoDialog implements FingerprintHandler.Callback {
 
-  private FingerprintManager.CryptoObject mCryptoObject;
-  private DialogResultHandler dialogCallback;
-  private FingerprintHandler mFingerprintHandler;
+    private FingerprintManager.CryptoObject mCryptoObject;
+    private DialogResultHandler dialogCallback;
+    private FingerprintHandler mFingerprintHandler;
 
-  void setContext(ReactApplicationContext context){
-    this.mFingerprintHandler = new FingerprintHandler(context, this);
-  }
+    void setContext(ReactApplicationContext context) {
+        this.mFingerprintHandler = new FingerprintHandler(context, this);
+    }
 
-  void setCryptoObject(FingerprintManager.CryptoObject cryptoObject) {
-    this.mCryptoObject = cryptoObject;
-  }
+    void setCryptoObject(FingerprintManager.CryptoObject cryptoObject) {
+        this.mCryptoObject = cryptoObject;
+    }
 
-  void setCallback(DialogResultHandler newDialogCallback) {
-    this.dialogCallback = newDialogCallback;
-  }
+    void setCallback(DialogResultHandler newDialogCallback) {
+        this.dialogCallback = newDialogCallback;
+    }
 
-  void startAuth() {
-    this.mFingerprintHandler.startAuth(mCryptoObject);
-  }
+    void startAuth() {
+        this.mFingerprintHandler.startAuth(mCryptoObject);
+    }
 
-  public void onAuthenticated() {
-    this.dialogCallback.onAuthenticated();
-  }
+    public void onAuthenticated() {
+        this.dialogCallback.onAuthenticated();
+    }
 
-  public void onError(String errorString, int errorCode) {
-      this.dialogCallback.emitErrorMessage(errorString, errorCode);
-  }
+    public void onError(String errorString, int errorCode) {
+        this.dialogCallback.emitErrorMessage(errorString, errorCode);
+    }
 
-  public void onCancelled() {
-    this.dialogCallback.onCancelled();
-    this.mFingerprintHandler.endAuth();
-  }
+    public void onCancelled() {
+        this.dialogCallback.onCancelled();
+        this.mFingerprintHandler.endAuth();
+    }
 }
