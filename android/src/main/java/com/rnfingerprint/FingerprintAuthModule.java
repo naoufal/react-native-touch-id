0c8ad0ab028b533b42e18c814eb1c61a5c82ce34
Diogo Moreira <dmoreira@uphold.com>
2019-03-24 19:46:22 +0000

    Dismiss dialog fragment on onPause

--- android/src/main/java/com/rnfingerprint/FingerprintAuthModule.java
+++ android/src/main/java/com/rnfingerprint/FingerprintAuthModule.java
@@ -20,6 +20,7 @@ public class FingerprintAuthModule extends ReactContextBaseJavaModule implements
 
     private static final String FRAGMENT_TAG = "fingerprint_dialog";
 
+    private FingerprintDialog fingerprintDialog;
     private KeyguardManager keyguardManager;
     private boolean isAppActive;
 
@@ -27,6 +28,8 @@ public class FingerprintAuthModule extends ReactContextBaseJavaModule implements
 
     public FingerprintAuthModule(final ReactApplicationContext reactContext) {
         super(reactContext);
+
+        fingerprintDialog = new FingerprintDialog();
 
         reactContext.addLifecycleEventListener(this);
     }
@@ -69,6 +72,13 @@ public class FingerprintAuthModule extends ReactContextBaseJavaModule implements
     @ReactMethod
     public void authenticate(final String reason, final ReadableMap authConfig, final Callback reactErrorCallback, final Callback reactSuccessCallback) {
         final Activity activity = getCurrentActivity();
+
+        if (inProgress && fingerprintDialog != null && activity != null) {
+          fingerprintDialog.show(activity.getFragmentManager(), FRAGMENT_TAG);
+
+          return;
+        }
+
         if (inProgress || !isAppActive || activity == null) {
             return;
         }
@@ -96,7 +106,6 @@ public class FingerprintAuthModule extends ReactContextBaseJavaModule implements
 
         final DialogResultHandler drh = new DialogResultHandler(reactErrorCallback, reactSuccessCallback);
 
-        final FingerprintDialog fingerprintDialog = new FingerprintDialog();
         fingerprintDialog.setCryptoObject(cryptoObject);
         fingerprintDialog.setReasonForAuthentication(reason);
         fingerprintDialog.setAuthConfig(authConfig);
