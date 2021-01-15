95cd95cd86fbf324d6f9a3558e65134a9ea1581c
i.yeryomin <ilya.yeryomin@gmail.com>
2021-01-15 03:26:02 +0200

    Fix layout

--- android/src/main/java/com/rnfingerprint/BiometricBackground.java
+++ android/src/main/java/com/rnfingerprint/BiometricBackground.java
@@ -8,10 +8,13 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.Window;
+import android.widget.ImageView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.DialogFragment;
+
+import com.bumptech.glide.Glide;
 
 public class BiometricBackground extends DialogFragment {
 
@@ -36,6 +39,8 @@ public class BiometricBackground extends DialogFragment {
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fingerprint_dialog, container, false);
+        ImageView logo = v.findViewById(R.id.logo);
+        Glide.with(requireContext()).load("https://www.managebac.com/wp-content/uploads/2020/07/ManageBac-vertical@2x-1024x758-1.png").into(logo);
         return v;
     }
 
