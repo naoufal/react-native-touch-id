d3920923543422044ea54dbf07ad0ea524c82a35
Alexander Danmayer <A.Danmayer@innologix.at>
2021-03-03 11:31:21 +0100

    add reactModule name

--- android/src/main/java/com/rnfingerprint/FingerprintAuthModule.java
+++ android/src/main/java/com/rnfingerprint/FingerprintAuthModule.java
@@ -21,10 +21,11 @@ import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
 import com.facebook.react.bridge.ReadableMap;
+import com.facebook.react.module.annotations.ReactModule;
 
 import java.util.concurrent.Executor;
 
-
+@ReactModule(name = FingerprintAuthModule.NAME)
 public class FingerprintAuthModule extends ReactContextBaseJavaModule implements LifecycleEventListener, RetryCallback {
     public static final String NAME = "FingerprintAuth";
     private static final String FRAGMENT_TAG = "fingerprint_dialog";
